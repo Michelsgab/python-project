d21ccc9949cfcdd1477cf984060ca58f5de4fdb5
Michelsgab <eugabmichels@gmail.com>
2022-06-22 17:44:42 -0300

    String Part 1

---
+++ Fundamentos/tipostring.py
@@ -0,0 +1,20 @@
+dir(str)
+
+nome = 'Gabriel Michels'
+# print(nome[0]) - String são imutáveis
+
+# marca 'dÁgua
+print("Dias D'Avilla" == 'Dias D\'Avilla')
+
+texto = 'Texto entre apostrófes pode ter "aspas"'
+texto2 = "Texto em aspas pode ter 'apostrófes'"
+
+print(texto, texto2)
+
+print("""
+Esse texto está
+utilizando múltiplas linhas
+em python
+""")
+
+print('Outra forma de utilizar múltiplas\nlinhas')
