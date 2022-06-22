1ecd1864c9cbb658a7232a81efa89cfc0c37a207
Michelsgab <eugabmichels@gmail.com>
2022-06-22 17:22:02 -0300

    Numeric Types

---
+++ Fundamentos/tiposnumericos.py
@@ -0,0 +1,22 @@
+print(dir(int))
+print(dir(float))
+
+a = 5
+b = 2.5
+
+print(a / b)
+print(a + b)
+print(a * b)
+
+print(type(a))
+print(type(b))
+print(type(a - b))
+
+b.is_integer()
+5.0.is_integer()
+
+print(dir(int))
+print(int.__add__(2, 5))
+print(2 + 5)
+
+print(abs(-2))
