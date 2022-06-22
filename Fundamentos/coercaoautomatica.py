f48f9da3b184e76b5ca80dcedbf2538403bb34c8
Michelsgab <eugabmichels@gmail.com>
2022-06-22 17:11:33 -0300

    Automatic conversion

---
+++ Fundamentos/coercaoautomatica.py
@@ -0,0 +1,13 @@
+print(10 / 2)
+print(type(10 / 2))
+print(type(10 / 3))
+
+print(10 // 3)
+print(type(10 // 3))
+
+print(10 // 3.3)
+print(type(10 // 3.3))
+
+print(2 + True)
+print(2 + False)
+print(int(True))
