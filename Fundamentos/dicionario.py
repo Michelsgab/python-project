056da82fbc733c23c1b59222a4e79c033b188c76
Michelsgab <eugabmichels@gmail.com>
2022-06-23 10:52:54 -0300

    Dict Part 2

--- Fundamentos/dicionario.py
+++ Fundamentos/dicionario.py
@@ -1,3 +1,4 @@
+# Part 1
 pessoa = {'nome': 'Gabriel', 'idade': 18, 'cursos': ['Inglês', 'Web']}
 print(type(pessoa))
 len(pessoa)
@@ -13,3 +14,16 @@ pessoa.items()
 pessoa.get('idade')
 
 pessoa.get('tags', [])
+
+# Part 2
+pessoa2 = {'nome': 'Prof. Glaciete', 'idade': 63, 'cursos': ['Matemática']}
+pessoa2['idade'] = 44
+pessoa2['cursos'].append('Geometria')
+print(pessoa2)
+pessoa2.pop('idade')
+print(pessoa2)
+pessoa2.update({'idade': 64, 'sexo': 'F'})
+del pessoa2['cursos']
+print(pessoa2)
+pessoa2.clear()
+print(pessoa2)
