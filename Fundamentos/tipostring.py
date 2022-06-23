fc0ebd4feacb389a9ad04e9f231a5c767d553dc5
Michelsgab <eugabmichels@gmail.com>
2022-06-23 10:13:41 -0300

    String Part 4

--- Fundamentos/tipostring.py
+++ Fundamentos/tipostring.py
@@ -44,3 +44,14 @@ print(frase.lower())
 
 print(frase.split())
 print(frase.split('e'))
+
+# Part 4
+a = '123'
+b = 'de Oliveira 4'
+print(a.__add__(b))
+print(str.__add__(a, b))
+
+len(a)
+print(a.__len__())
+print('1' in a)
+print(a.__contains__('1'))
