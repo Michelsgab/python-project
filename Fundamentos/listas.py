911db521c587f50de49a5ea50ee6e573d3c56823
Michelsgab <eugabmichels@gmail.com>
2022-06-23 10:26:13 -0300

    List Part 3

--- Fundamentos/listas.py
+++ Fundamentos/listas.py
@@ -26,3 +26,17 @@ lista2[2]
 'Pedro' not in lista2
 lista[-1]
 lista[-5]
+
+# Part 3
+lista3 = ['Ana', 'Lia', 'Rui', 'Paulo', 'Dani']
+lista3[1:3]
+lista3[1:-1]
+lista3[1:]
+lista3[:-1]
+lista3[:]
+lista3[::2]
+lista3[::-1]
+del lista3[2]
+lista3
+del lista3[1:]
+lista3
