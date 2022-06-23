f0b5262d3d1c6227f2f783da060a4cead3063ce6
Michelsgab <eugabmichels@gmail.com>
2022-06-23 14:49:29 -0300

    Set

---
+++ Fundamentos/conjuntos.py
@@ -0,0 +1,18 @@
+a = {1, 2, 3}
+print(type(a))
+
+a = set('magna')
+print(a)
+print('a' in a, 'f' not in a)
+{1, 2, 3} == {3, 2, 1, 2} # True
+
+c1 = {1, 2}
+c2 = {2, 3}
+print(c1.union(c2))
+print(c1.intersection(c2))
+c1.update(c2)
+print(c1)
+
+print(c2 <= c1)
+
+{1, 2, 3} - {2}
