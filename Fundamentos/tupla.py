1b51ba583a328aebc8d063e77585038e3274ae23
Michelsgab <eugabmichels@gmail.com>
2022-06-23 10:31:05 -0300

    Tuple

---
+++ Fundamentos/tupla.py
@@ -0,0 +1,12 @@
+tupla = tuple()
+tupla = ()
+print(type(tupla))
+tupla = ('um',)
+tupla=[0]
+# tupla[0] = 'novo'
+cores = ('verde', 'amarelo', 'azul', 'branco')
+cores[0]
+cores[-1]
+cores[:1]
+cores.index('amarelo')
+cores.count('azul')
