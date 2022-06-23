70d3316aa1aab27ae24b890587f42b502869e683
Michelsgab <eugabmichels@gmail.com>
2022-06-23 10:23:25 -0300

    List Part 2

--- Fundamentos/listas.py
+++ Fundamentos/listas.py
@@ -16,3 +16,13 @@ nova_lista.remove(2)
 print(nova_lista)
 nova_lista.reverse()
 print(nova_lista)
+
+# Part 2
+lista2 = [1, 5, 'Rebeca', 'Guilherme', 3.1415]
+print(lista2.index('Guilherme'))
+lista2[2]
+1 in lista2
+'Receba' in lista2
+'Pedro' not in lista2
+lista[-1]
+lista[-5]
