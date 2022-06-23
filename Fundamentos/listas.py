625f382e6c7d949cf43b03c897689d90ed85dd66
Michelsgab <eugabmichels@gmail.com>
2022-06-23 10:19:42 -0300

    List Part 1

---
+++ Fundamentos/listas.py
@@ -0,0 +1,18 @@
+# Parte 1
+lista = []
+print(type(lista))
+
+#help(list)
+
+print(len(lista))
+lista.append(1)
+lista.append(5)
+print(lista)
+print(len(lista))
+
+nova_lista = ['Ana', 'Bia', 1, 2]
+print(nova_lista)
+nova_lista.remove(2)
+print(nova_lista)
+nova_lista.reverse()
+print(nova_lista)
