22078fe405078e1aab3c7221b60656bd499e5eb0
Michelsgab <eugabmichels@gmail.com>
2022-06-24 12:34:55 -0300

    Decorator

---
+++ funcoes/decorator.py
@@ -0,0 +1,24 @@
+#!/usr/local/bin/python3
+def log(function):
+    def decorator(*args, **kwargs):
+        print(f'Inicio da chamada da função: {function.__name__}')
+        print(f'args: {args}')
+        print(f'kwargs: {kwargs}')
+        resultado = function(*args, **kwargs)
+        print(f'Resultado da chamada: {resultado}')
+        return resultado
+    return decorator
+
+@log
+def soma(x, y):
+    return x + y
+
+
+@log
+def subtracao(x, y):
+    return x - y
+
+
+if __name__ == '__main__':
+    print(soma(5, 13))
+    print(subtracao(13, y=5))
