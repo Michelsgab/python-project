2167c0338cd6e677550c0cebd0d834fb6e68977c
Michelsgab <eugabmichels@gmail.com>
2022-06-27 09:02:22 -0300

    O.O.P Advanced

---
+++ poo_avancado/evolucao_v3.py
@@ -0,0 +1,44 @@
+#!/usr/local/bin/python3
+class Humano:
+    # Atributo de classe
+    especie = 'Homo Sapiens'
+
+    def __init__(self, nome):
+        self.nome = nome
+        self._idade = None
+
+    def get_idade(self):
+        return self._idade
+
+    def set_idade(self, idade):
+        if idade < 0:
+            raise ValueError('Idade inválida')
+        self._idade = idade
+
+    def das_cavernas(self):
+        self.especie = 'Homo Neanderthalensis'
+        return self
+
+    @staticmethod
+    def especies():
+        adjetivos = ('Habilis', 'Erectus', 'Neanderthalensis', 'Sapiens')
+        return ('Australopiteco',) + tuple(f'Homo {adj}' for adj in adjetivos)
+
+    @classmethod
+    def is_evoluido(cls):
+        return cls.especie == cls.especies()[-1]
+
+
+class Neanderthal(Humano):
+    especie = Humano.especies()[-2]
+
+
+class HomoSapiens(Humano):
+    especie = Humano.especies()[-1]
+
+
+if __name__ == '__main__':
+    jose = HomoSapiens('José')
+    jose.set_idade(40)
+    print(f'Nome: {jose.nome}, Idade: {jose.get_idade()}')
+    grokn = Neanderthal('Grokn')
